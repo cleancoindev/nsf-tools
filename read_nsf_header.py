cfce5d5860935857210a710a237b740ada7a331d
Alex Rasmussen <alexras@acm.org>
2013-06-06 01:09:46 -0700

    Simple tool that uses alexras/bread to parse NSF file headers

---
+++ read_nsf_header.py
@@ -0,0 +1,38 @@
+import bread as b
+import sys
+
+def hex_array(x):
+    return str(map(hex, x))
+
+nsf_header = [
+    ('magic_number', b.array(5, b.byte),
+     {"str_format": hex_array}),
+    ('version', b.byte),
+    ('total_songs', b.byte),
+    ('starting_song', b.byte),
+    ('load_addr', b.uint16, {"str_format": hex}),
+    ('init_addr', b.uint16, {"str_format": hex}),
+    ('play_addr', b.uint16, {"str_format": hex}),
+    ('title', b.string(32)),
+    ('artist', b.string(32)),
+    ('copyright', b.string(32)),
+    ('ntsc_speed', b.uint16),
+    ('bankswitch_init', b.array(8, b.byte), {"str_format": hex_array}),
+    ('pal_speed', b.uint16),
+    ('ntsc', b.boolean),
+    ('pal', b.boolean),
+    ('ntsc_and_pal', b.boolean),
+    (b.padding(6)),
+    ('vrc6', b.boolean),
+    ('vrc7', b.boolean),
+    ('fds', b.boolean),
+    ('mmc5', b.boolean),
+    ('namco_106', b.boolean),
+    ('fme07', b.boolean),
+    (b.padding(2)),
+    (b.padding(32))
+]
+
+with open(sys.argv[1], 'r') as fp:
+    header = b.parse(fp, nsf_header)
+    print header
